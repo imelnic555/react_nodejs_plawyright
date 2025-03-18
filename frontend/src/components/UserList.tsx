c7feebbb85bf41007a9a0f4baf916b39606fe15f
imelnic <imelnic555@gmail.com>
2025-03-18 12:39:22 +0200

    fixed frontesnd1

--- frontend/src/components/UserList.tsx
+++ frontend/src/components/UserList.tsx
@@ -27,12 +27,12 @@ const UserList: React.FC = () => {
 
     return (
         <div>
-            <h2>User List</h2>
-            <ul>
-                {users?.map((user) => (
-                    <li key={user.id}>{user.name} - {user.email}</li>
-                ))}
-            </ul>
+            {/*<h2>User List</h2>*/}
+            {/*<ul>*/}
+            {/*    {users?.map((user) => (*/}
+            {/*        <li key={user.id}>{user.name} - {user.email}</li>*/}
+            {/*    ))}*/}
+            {/*</ul>*/}
         </div>
     );
 };
