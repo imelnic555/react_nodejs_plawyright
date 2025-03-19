1345cb6a1115102d7ea3453e94fb3db1ec18cf0d
imelnic <imelnic555@gmail.com>
2025-03-19 10:36:10 +0200

    fixed frontesnd1

--- tests/cucumber.cjs
+++ tests/cucumber.cjs
@@ -1,11 +1,11 @@
 module.exports = {
     default: {
         require: [
-            "ts-node/register/transpile-only",  // Skip type-checking for faster transpilation
-            "./support/hooks.ts",
-            "./steps/**/*.ts"
+            "ts-node/register",         // Transpile TS files on the fly (with type-checking)
+            "./support/hooks.ts",       // Load hooks
+            "./steps/**/*.ts"           // Load step definitions recursively
         ],
-        paths: ["./features/*.feature"],
+        paths: ["./features/*.feature"],  // Feature files location
         format: ["progress"],
         worldParameters: {}
     }
